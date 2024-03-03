67e23890e38bd1933fa330970708911a9896c7d3
valeria p <pikulvaleria@yandex.tu>
2024-03-03 14:20:02 +0300

    edits 1

--- README.md
+++ README.md
@@ -3,6 +3,15 @@
 <a href="https://codeclimate.com/github/valeriapikul/frontend-project-44/maintainability"><img src="https://api.codeclimate.com/v1/badges/d849d712b90ec4022d20/maintainability" /></a>
 <a href="https://codeclimate.com/github/valeriapikul/frontend-project-44/test_coverage"><img src="https://api.codeclimate.com/v1/badges/d849d712b90ec4022d20/test_coverage" /></a>
 
+## Setup
+```bash
+make install
+```
+
+```bash
+Первый проект Хекслета "Игры разума", включающий в себя 5 игр: Проверка на чётность, Калькулятор, НОД, Арифметическая прогрессия и Простое ли число?
+```
+
 ### My records:
 <a href="https://asciinema.org/a/4YUIR7YW42NG5Hr0fMcvHI9pk" target="_blank"><img src="https://asciinema.org/a/4YUIR7YW42NG5Hr0fMcvHI9pk.svg" /></a>
 <a href="https://asciinema.org/a/JC3WNrWl5GJBgDYvEvhRKrhEF" target="_blank"><img src="https://asciinema.org/a/JC3WNrWl5GJBgDYvEvhRKrhEF.svg" /></a>
--- src/index.js
+++ src/index.js
@@ -1,27 +1,56 @@
 import readlineSync from 'readline-sync';
 
-export const generateRandomNum = (placeNumber = 10) => Math.round(Math.random() * placeNumber);
+// export const generateRandomNum = (placeNumber = 10) => Math.round(Math.random() * placeNumber);
 
-export const generateRandomMinMax = (min, max) => Math.round(Math.random() * (max - min) + min);
+// export const generateRandomMinMax = (min, max) => Math.round(Math.random() * (max - min) + min);
 
-export const getAnswer = () => readlineSync.question('Your answer: ');
+// export const getAnswer = () => readlineSync.question('Your answer: ');
 
-export const compareAnswers = (corrAns, yourAns, userName) => {
-  if (corrAns === yourAns) {
-    console.log('Correct!');
-  } else {
-    console.log(`${yourAns} is wrong answer ;(. Correct answer was ${corrAns}.\nLet's try again, ${userName}!`);
-    return false;
+// export const compareAnswers = (corrAns, yourAns, userName) => {
+//   if (corrAns === yourAns) {
+//     console.log('Correct!');
+//   } else {
+//     console.log(`${yourAns} is wrong answer ;(. Correct answer was ${corrAns}.\nLet's try again, ${userName}!`);
+//     return false;
+//   }
+//   return true;
+// };
+
+// export const congratulations = (userName) => {
+//   console.log(`Congratulations, ${userName}!`);
+// };
+
+// export const generateRandomOperator = () => {
+//   const arr = ['+', '-', '*'];
+//   const j = Math.floor(Math.random() * arr.length);
+//   return arr[j];
+// };
+
+
+
+
+const runEngine = (rules, generateRound) => {
+  console.log('Welcome to the Brain Games!');
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
+
+  console.log(rules);
+
+  const roundsCount = 3;
+
+  for (let i = 0; i < roundsCount; i++) {
+    const [question, answer] = generateRound();
+
+    console.log(question);
+
+    answer =  readlineSync.question('Your answer: ');
+
+    if (question === answer) {
+      console.log('Correct!');
+    } else {
+      console.log(`${answer} is wrong answer ;(. Correct answer was ${question}.\nLet's try again, ${name}!`);
+      return false;
+    }
   }
-  return true;
-};
-
-export const congratulations = (userName) => {
-  console.log(`Congratulations, ${userName}!`);
-};
-
-export const generateRandomOperator = () => {
-  const arr = ['+', '-', '*'];
-  const j = Math.floor(Math.random() * arr.length);
-  return arr[j];
-};
+  console.log(`Congratulations, ${name}!`);
+}
